9806f6960b4a109169e9b8f8fe03feace169a35e
Alexander Ostrovsky <thealeos@gmail.com>
2022-09-08 11:32:40 +0300

    Add a sample project

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Alexander Ostrovsky on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
